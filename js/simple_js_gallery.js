839cc8396718e049e639cc37072447fa1b87bf79
James Gallichio <james@agoodman.com.au>
2012-12-11 22:53:39 +1100

    Added selector so script is optional

--- js/simple_js_gallery.js
+++ js/simple_js_gallery.js
@@ -1,25 +1,29 @@
 //Javascript Image Changer
+function simple_js_gallery(element){
 
-var currentImage = document.getElementById("currentImage");
+	var selectedElement = document.getElementById(element);
 
-var imageArray = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"]; //Put images in an array
+	var currentImage = selectedElement.getElementsByTagName("img")[0];
 
-var imageIndex= 0;
+	var imageArray = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"]; //Put images in an array
 
-function changeImage(param){
-	imageIndex += param;
-	var max = imageArray.length;
-	if (imageIndex >= max){
-		imageIndex = 0;
-	}else if(imageIndex < 0){
-		imageIndex = max -1;
+	var imageIndex= 0;
+
+	function changeImage(param){
+		imageIndex += param;
+		var max = imageArray.length;
+		if (imageIndex >= max){
+			imageIndex = 0;
+		}else if(imageIndex < 0){
+			imageIndex = max -1;
+		}
+		currentImage.setAttribute("src", imageArray[imageIndex]);
 	}
-	currentImage.setAttribute("src", imageArray[imageIndex]);
-}
 
-var controlLeft = document.getElementById("left");
-var controlRight = document.getElementById("right");
+	var controlLeft = document.getElementById("left");
+	var controlRight = document.getElementById("right");
 
 
-controlLeft.onclick = function() { changeImage(-1); };
-controlRight.onclick = function() { changeImage(1); };
+	controlLeft.onclick = function() { changeImage(-1); };
+	controlRight.onclick = function() { changeImage(1); };
+};
