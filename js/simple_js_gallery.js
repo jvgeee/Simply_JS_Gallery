ce0b3760cc06a5c660ee29c2ea4739ef37989afa
James Gallichio <james@agoodman.com.au>
2012-12-11 22:19:30 +1100

    Got rid of debugging stuff

--- js/simple_js_gallery.js
+++ js/simple_js_gallery.js
@@ -2,20 +2,18 @@
 
 var currentImage = document.getElementById("currentImage");
 
-var imageArray = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
+var imageArray = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"]; //Put images in an array
 
 var imageIndex= 0;
 
 function changeImage(param){
 	imageIndex += param;
 	var max = imageArray.length;
-	console.log(max);
 	if (imageIndex >= max){
 		imageIndex = 0;
 	}else if(imageIndex < 0){
 		imageIndex = max -1;
 	}
-	console.log(imageIndex);
 	currentImage.setAttribute("src", imageArray[imageIndex]);
 }
 
